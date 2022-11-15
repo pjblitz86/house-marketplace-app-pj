733f432dfb2709aa3a694c3fb399ede52786fa3f
pjblitz86 <paulius.juknis@gmail.com>
2022-11-15 09:15:03 +0200

    initial react app and firebase setup

---
+++ src/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: 'house-marketplace-pj.firebaseapp.com',
+  projectId: 'house-marketplace-pj',
+  storageBucket: 'house-marketplace-pj.appspot.com',
+  messagingSenderId: '575583155572',
+  appId: '1:575583155572:web:d625313559207bf00ae821'
+};
+
+initializeApp(firebaseConfig);
+
+export const db = getFirestore();
