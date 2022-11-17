2a156c9b3b03f86f0c3502ba674118dfaf32808e
pjblitz86 <paulius.juknis@gmail.com>
2022-11-17 10:41:02 +0200

    save user to firestore db

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,6 +5,7 @@ import {
   createUserWithEmailAndPassword,
   updateProfile
 } from 'firebase/auth';
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore';
 import { db } from '../firebase.config';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
@@ -39,6 +40,12 @@ function SignUp() {
 
       const user = userCredential.user;
       updateProfile(auth.currentUser, { displayName: name });
+
+      const formDataCopy = { ...formData };
+      delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
+
+      await setDoc(doc(db, 'users', user.uid), formDataCopy);
 
       navigate('/');
     } catch (error) {
